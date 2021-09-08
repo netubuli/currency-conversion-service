9d2468fc44eac37efb1db30a4a06926eb815c84e
netubuli <netubuli@gmail.com>
2021-09-08 16:42:01 +0300

    slight description added to main class

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,13 +2,13 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*this is the raw class before adding other extensios or configurations*/
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
-		//main application class
+		
 	}
 
 }
