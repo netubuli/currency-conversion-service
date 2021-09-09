41136d44c2527081f7e84be2be8376f2369fb4b3
netubuli <netubuli@gmail.com>
2021-09-09 17:27:06 +0300

    rest template code added

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,7 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*this is the raw class before adding other extensios or configurations*/
+/*this is the raw class before adding other extensios or configurations */
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
